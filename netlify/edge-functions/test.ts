890ee07d2de23e8571c801b593641098210d7e75
mccarthyk <mccarthyk@hcflgov.net>
2022-11-03 17:26:20 -0400

    Update test.ts

--- netlify/edge-functions/test.ts
+++ netlify/edge-functions/test.ts
@@ -2,12 +2,12 @@ import type { Request, Context } from 'https://edge.netlify.com'
 
 export default async (req: Request, context: Context) => {
   const { pathname } = new URL('./.deps/test.gql', import.meta.url)
-  //   const query = await Deno.readTextFile(pathname)
+  const query = await Deno.readTextFile(pathname)
 
   return context.json({
     //
     meta: import.meta.url,
     pathname,
-    // query,
+    query,
   })
 }
