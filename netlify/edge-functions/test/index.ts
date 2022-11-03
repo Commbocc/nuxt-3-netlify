e1f85885f4731597bc7bf9b23fac3de57babaf57
mccarthyk <mccarthyk@hcflgov.net>
2022-11-03 18:56:46 -0400

    Update index.ts

--- netlify/edge-functions/test/index.ts
+++ netlify/edge-functions/test/index.ts
@@ -4,7 +4,7 @@ export default async (req: Request, context: Context) => {
   const { pathname } = new URL('./test.gql', import.meta.url)
   
   // const query = await Deno.readTextFile(pathname)
-  const query = await Deno.readTextFile('./netlify/edge-functions/test/test.gql')
+  const query = await Deno.readTextFile(import.meta.url)
   
 
   return context.json({
