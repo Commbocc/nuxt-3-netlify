d33a21a021715538f3a65e441175dc8739d5e005
mccarthyk <mccarthyk@hcflgov.net>
2022-11-03 17:16:18 -0400

    Create test.ts

---
+++ netlify/edge-functions/test.ts
@@ -0,0 +1,13 @@
+import type { Request, Context } from 'https://edge.netlify.com'
+
+export default async (req: Request, context: Context) => {
+  //   const { pathname } = new URL('./.deps/test.gql', import.meta.url)
+  //   const query = await Deno.readTextFile(pathname)
+
+  return context.json({
+    //
+    meta: import.meta.url,
+    // pathname,
+    // query,
+  })
+}
