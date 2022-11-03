7e2390aeb7f5a65257a6395e515ee613556ca004
mccarthyk <mccarthyk@hcflgov.net>
2022-11-03 18:45:00 -0400

    Update and rename netlify/edge-functions/test.ts to netlify/edge-functions/test/index.ts

--- netlify/edge-functions/test/index.ts
+++ netlify/edge-functions/test/index.ts
@@ -1,13 +1,14 @@
 import type { Request, Context } from 'https://edge.netlify.com'
 
 export default async (req: Request, context: Context) => {
-  const { pathname } = new URL('./.deps/test.gql', import.meta.url)
-  const query = await Deno.readTextFile(pathname)
+  const { pathname } = new URL('./test.gql', import.meta.url)
+  
+  // const query = await Deno.readTextFile(pathname)
 
   return context.json({
     //
     meta: import.meta.url,
     pathname,
-    query,
+    // query,
   })
 }
