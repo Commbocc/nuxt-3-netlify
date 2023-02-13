90a6e9f1a28eeaa1fadcebec2cb8c7962fe7dc22
mccarthyk <mccarthyk@hcflgov.net>
2023-02-13 09:09:05 -0500

    localfetch

--- server/routes/ical.ics.ts
+++ server/routes/ical.ics.ts
@@ -1,13 +1,15 @@
-import axios from 'axios'
+// import axios from 'axios'
 
 export default defineEventHandler(async (event) => {
   //   setHeader(event, 'Content-Type', 'text/plain')
 
-  const { host } = event.node.req.headers
+  // const { host } = event.node.req.headers
 
-  const url = `//${host}/test.Query.gql`
+  // const url = `//${host}/test.Query.gql`
+  // const { data: query } = await axios.get(url)
 
-  const { data: query } = await axios.get(url)
+  const { localFetch } = useNitroApp()
+  const query = await localFetch('/test.Query.gql', {}).then((r) => r.text())
 
-  return { host, url, query }
+  return { query }
 })
